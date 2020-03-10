db6542dc25b28b0b2022a5f2b8a08305e5ffb0bb
vi.vu001 <viet.vu001@stud.fh-dortmund.de>
2020-03-10 23:32:50 +0100

    added cardChild for ReusableCard and it's icon

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\vdvu\Downloads\Software\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\vdvu\Downloads\Projects\bmi_project"
+export "FLUTTER_ROOT=D:\Setup\Lernen\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\flutter.projects\bmi_calculation"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\vdvu\Downloads\Software\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Setup\Lernen\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Color cardColor = Color(0xFFC7493A);
+const Color iconColor = Color(0xFFDCDCDA);
+const Color activeCardColor = Color(0xFFFFFFFF);
+const Color bottomContainerColor = Color(0xFFFFCF70);
 
 class InputPage extends StatefulWidget {
   @override
@@ -10,34 +16,47 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
+      appBar: AppBar(title: Text('BMI CALCULATOR')),
       body: Column(
         children: <Widget>[
           Expanded(
             child: Row(children: <Widget>[
               Expanded(
-                child: ReusableCard(colour: Color(0xFFC7493A)),
+                child: ReusableCard(
+                  colour: activeCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.mars, size: 85.0, color: iconColor),
+//                      SizedBox(height: 20.0),
+                      Text('MALE',
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: iconColor,
+                              fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ),
               ),
-              Expanded(child: ReusableCard(colour: Color(0xFFC7493A))),
+              Expanded(child: ReusableCard(colour: cardColor))
             ]),
-          ),
-          Expanded(
-            child: Row(children: <Widget>[Expanded(child: ReusableCard(colour: Color(0xFFC7493A)))]),
           ),
           Expanded(
             child: Row(children: <Widget>[
-              Expanded(
-                child: ReusableCard(colour: Color(0xFFC7493A)),
-              ),
-              Expanded(
-                child: ReusableCard(
-                  colour: Colors.amber
-                ),
-              ),
+              Expanded(child: ReusableCard(colour: cardColor))
             ]),
           ),
+          Expanded(
+            child: Row(children: <Widget>[
+              Expanded(child: ReusableCard(colour: cardColor)),
+              Expanded(child: ReusableCard(colour: cardColor))
+            ]),
+          ),
+          Container(
+              color: Color(0xFFFFCF70),
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80.0)
         ],
       ),
     );
@@ -45,16 +64,17 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
- Color colour;
+  final Color colour;
+  final Widget cardChild;
 
- ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+        child: cardChild,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
-            color: colour,
-            borderRadius: BorderRadius.circular(10.0)));
+            color: colour, borderRadius: BorderRadius.circular(10.0)));
   }
 }
